0c8b93a08d803df999793b9dad73b7b726e9d34d
Connor Runyan <connorrunyan@email.arizona.edu>
2018-01-13 16:19:17 -0700

    Update hack.py

--- hack.py
+++ hack.py
@@ -54,7 +54,7 @@ def onPress():
   voice = client2.synthesize_speech(
     OutputFormat='mp3',
     SampleRate='22050',
-    Text='Taking Picture',
+    Text='Nothing Recognized',
     TextType='text',
     #VoiceId='Geraint'|'Gwyneth'|'Mads'|'Naja'|'Hans'|'Marlene'|'Nicole'|'Russell'|'Amy'|'Brian'|'Emma'|'Raveena'|'Ivy'|'Joanna'|'Joey'|'Justin'|'Kendra'|'Kimberly'|'Matthew'|'Salli'|'Conchita'|'Enrique'|'Miguel'|'Penelope'|'Chantal'|'Celine'|'Mathieu'|'Dora'|'Karl'|'Carla'|'Giorgio'|'Mizuki'|'Liv'|'Lotte'|'Ruben'|'Ewa'|'Jacek'|'Jan'|'Maja'|'Ricardo'|'Vitoria'|'Cristiano'|'Ines'|'Carmen'|'Maxim'|'Tatyana'|'Astrid'|'Filiz'|'Vicki'|'Takumi'|'Seoyeon'|'Aditi'
     VoiceId='Matthew'
@@ -66,7 +66,7 @@ def onPress():
   
   print(voiceBytes)
   
-  with open('taking.mp3', 'wb') as w:
+  with open('nothing.mp3', 'wb') as w:
     w.write(voiceBytes)
     
   pygame.init()
