d337a5d290c5f8a73ed548e3dc09018fcef7a952
Connor Runyan <connorrunyan@email.arizona.edu>
2018-01-12 23:49:31 -0700

    Update hack.py

--- hack.py
+++ hack.py
@@ -5,13 +5,6 @@ import pygame.image
 
 # wait for button press here, when it happens call onPress
 
-sudo apt-get install mercurial python3-dev python3-setuptools python3-numpy python3-opengl \
-    libav-tools libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev \
-    libsdl1.2-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev \
-    libtiff5-dev libx11-6 libx11-dev fluid-soundfont-gm timgm6mb-soundfont \
-    xfonts-base xfonts-100dpi xfonts-75dpi xfonts-cyrillic fontconfig fonts-freefont-ttf
-
-
 def onPress():
   pygame.camera.init()
   cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
